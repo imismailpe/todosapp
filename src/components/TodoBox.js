67b7d8e01595b8107dde8ddf3892964ff54fd47f
Ismail <mailismailpe@gmail.com>
2021-10-06 11:13:00 +0530

    code quality

--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -5,7 +5,7 @@ const TodoBox = (props) => {
     return (
         <div className={`todosBox ${props.bgColor}`} onDragOver={props.onDragOver} onDrop={e => props.onDrop(e, props.status)} >
             <div className='todosBoxTitle'>{props.status}</div>
-            <div className='todosBoxBody'>
+            <div className='todosBoxBody hideOverflow'>
                 {
                     props.data.map(item => <div onDragStart={e => props.onDragStart(e,item.id)} className='todoItem todoItemContainer_' key={item.id} draggable='true'><li>{item.text}</li></div>)
                 }
